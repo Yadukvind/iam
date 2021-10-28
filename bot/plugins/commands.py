6432d7ea6e4f5d8438406958b2a4e8a1e020269d
Yadukvind <93067144+Yadukvind@users.noreply.github.com>
2021-10-28 09:10:10 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = @efcinema,
+                caption = @efchannel2,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
